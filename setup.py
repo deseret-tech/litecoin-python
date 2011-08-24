bf77bef71d2e6e5858e7a4ac2e6c18f28e52dd54
Witchspace <witchspace81@gmail.com>
2011-08-24 09:22:43 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "bitcoin-python",
-    version = "0.1",
+    version = "0.2",
     packages = find_packages("src"),
     package_dir = {'':'src'}
 )
