a9555618609e65ebb57444ba7d3958df9f017ed1
Alexander Petrovich <chopachom@gmail.com>
2012-07-15 12:07:53 +0400

    Added listsinceblock, getblock, getblockhash

--- src/bitcoinrpc/connection.py
+++ src/bitcoinrpc/connection.py
@@ -1,15 +1,15 @@
 # Copyright (c) 2010 Witchspace <witchspace81@gmail.com>
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,11 +28,11 @@ class BitcoinConnection(object):
     """
     A BitcoinConnection object defines a connection to a bitcoin server.
     It is a thin wrapper around a JSON-RPC API connection.
-    
+
     Up-to-date for SVN revision 198.
-    
+
     Arguments to constructor:
-    
+
     - *user* -- Authenticate as user.
     - *password* -- Authentication password.
     - *host* -- Bitcoin JSON-RPC host.
@@ -58,7 +58,16 @@ class BitcoinConnection(object):
             self.proxy.stop()
         except JSONRPCException,e:
             raise _wrap_exception(e.error)
-            
+
+
+    def getblock(self, hash):
+        """
+        """
+        try:
+            return self.proxy.getblock(hash)
+        except JSONRPCException,e:
+            raise _wrap_exception(e.error)
+
     def getblockcount(self):
         """
         Returns the number of blocks in the longest block chain.
@@ -66,8 +75,20 @@ class BitcoinConnection(object):
         try:
             return self.proxy.getblockcount()
         except JSONRPCException,e:
-            raise _wrap_exception(e.error)    
-        
+            raise _wrap_exception(e.error)
+
+    def getblockhash(self, index):
+        """
+        Returns hash of block in best-block-chain at index.
+
+        :param index: index ob the block
+
+        """
+        try:
+            return self.proxy.getblockhash(index)
+        except JSONRPCException as e:
+            raise _wrap_exception(e.error)
+
     def getblocknumber(self):
         """
         Returns the block number of the latest block in the longest block chain.
@@ -75,8 +96,8 @@ class BitcoinConnection(object):
         try:
             return self.proxy.getblocknumber()
         except JSONRPCException,e:
-            raise _wrap_exception(e.error)  
-                
+            raise _wrap_exception(e.error)
+
     def getconnectioncount(self):
         """
         Returns the number of connections to other nodes.
@@ -107,7 +128,7 @@ class BitcoinConnection(object):
     def setgenerate(self, generate, genproclimit=None):
         """
         Enable or disable generation (mining) of coins.
-        
+
         Arguments:
 
         - *generate* -- is :const:`True` or :const:`False` to turn generation on or off.
@@ -143,7 +164,7 @@ class BitcoinConnection(object):
     def getnewaddress(self, account=None):
         """
         Returns a new bitcoin address for receiving payments.
-        
+
         Arguments:
 
         - *account* -- If account is specified (recommended), it is added to the address book
@@ -161,9 +182,9 @@ class BitcoinConnection(object):
     def getaccountaddress(self, account):
         """
         Returns the current bitcoin address for receiving payments to an account.
-        
+
         Arguments:
-        
+
         - *account* -- Account for which the address should be returned.
 
         """
@@ -175,7 +196,7 @@ class BitcoinConnection(object):
     def setaccount(self, bitcoinaddress, account):
         """
         Sets the account associated with the given address.
-        
+
         Arguments:
 
         - *bitcoinaddress* -- Bitcoin address to associate.
@@ -190,9 +211,9 @@ class BitcoinConnection(object):
     def getaccount(self, bitcoinaddress):
         """
         Returns the account associated with the given address.
-        
+
         Arguments:
-        
+
         - *bitcoinaddress* -- Bitcoin address to get account for.
         """
         try:
@@ -203,22 +224,22 @@ class BitcoinConnection(object):
     def getaddressesbyaccount(self, account):
         """
         Returns the list of addresses for the given account.
-        
+
         Arguments:
-        
+
         - *account* -- Account to get list of addresses for.
-        """ 
+        """
         try:
-            return self.proxy.getaddressesbyaccount(account) 
+            return self.proxy.getaddressesbyaccount(account)
         except JSONRPCException,e:
             raise _wrap_exception(e.error)
 
     def sendtoaddress(self, bitcoinaddress, amount, comment=None, comment_to=None):
         """
         Sends *amount* from the server's available balance to *bitcoinaddress*.
-        
+
         Arguments:
-        
+
         - *bitcoinaddress* -- Bitcoin address to send to.
         - *amount* -- Amount to send (float, rounded to the nearest 0.01).
         - *minconf* -- Minimum number of confirmations required for transferred balance.
@@ -235,12 +256,12 @@ class BitcoinConnection(object):
                 return self.proxy.sendtoaddress(bitcoinaddress, amount, comment, comment_to)
         except JSONRPCException,e:
             raise _wrap_exception(e.error)
-  
+
     def getreceivedbyaddress(self, bitcoinaddress, minconf=1):
         """
         Returns the total amount received by a bitcoin address in transactions with at least a
         certain number of confirmations.
-        
+
         Arguments:
 
         - *bitcoinaddress* -- Address to query for total amount.
@@ -251,14 +272,14 @@ class BitcoinConnection(object):
             return self.proxy.getreceivedbyaddress(bitcoinaddress, minconf)
         except JSONRPCException,e:
             raise _wrap_exception(e.error)
-        
+
     def getreceivedbyaccount(self, account, minconf=1):
         """
-        Returns the total amount received by addresses with an account in transactions with 
+        Returns the total amount received by addresses with an account in transactions with
         at least a certain number of confirmations.
-        
+
         Arguments:
-        
+
         - *account* -- Account to query for total amount.
         - *minconf* -- Number of confirmations to require, defaults to 1.
 
@@ -267,8 +288,7 @@ class BitcoinConnection(object):
             return self.proxy.getreceivedbyaccount(account, minconf)
         except JSONRPCException,e:
             raise _wrap_exception(e.error)
-            
-            
+
     def gettransaction(self, txid):
         """
         Get detailed information about transaction
@@ -282,16 +302,24 @@ class BitcoinConnection(object):
             return TransactionInfo(**self.proxy.gettransaction(txid))
         except JSONRPCException,e:
             raise _wrap_exception(e.error)
-    
+
+
+    def listsinceblock(self, block_hash):
+        try:
+            res = self.proxy.listsinceblock(block_hash)
+            res['transactions'] = [TransactionInfo(**x) for x in res['transactions']]
+            return res
+        except JSONRPCException,e:
+            raise _wrap_exception(e.error)
 
     def listreceivedbyaddress(self, minconf=1, includeempty=False):
         """
         Returns a list of addresses.
-        
+
         Each address is represented with a :class:`~bitcoinrpc.data.AddressInfo` object.
 
         Arguments:
-        
+
         - *minconf* -- Minimum number of confirmations before payments are included.
         - *includeempty* -- Whether to include addresses that haven't received any payments.
 
@@ -300,13 +328,13 @@ class BitcoinConnection(object):
             return [AddressInfo(**x) for x in self.proxy.listreceivedbyaddress(minconf, includeempty)]
         except JSONRPCException,e:
             raise _wrap_exception(e.error)
-        
+
     def listaccounts(self, minconf=1):
         """
         Returns a list of account names.
-        
+
         Arguments:
-        
+
         - *minconf* -- Minimum number of confirmations before payments are included.
         """
         try:
@@ -315,17 +343,17 @@ class BitcoinConnection(object):
         except JSONRPCException,e:
             raise _wrap_exception(e.error)
 
-    
+
     def listreceivedbyaccount(self, minconf=1, includeempty=False):
         """
         Returns a list of accounts.
-        
+
         Each account is represented with a :class:`~bitcoinrpc.data.AccountInfo` object.
-        
+
         Arguments:
-        
+
         - *minconf* -- Minimum number of confirmations before payments are included.
-        
+
         - *includeempty* -- Whether to include addresses that haven't received any payments.
         """
         try:
@@ -333,21 +361,21 @@ class BitcoinConnection(object):
         except JSONRPCException,e:
             raise _wrap_exception(e.error)
 
-    def listtransactions(self, account='', count=10, address=None):
+    def listtransactions(self, account, count=10, address=None):
         """
         Returns a list of the last transactions for an account.
-        
+
         Each transaction is represented with a :class:`~bitcoinrpc.data.TransactionInfo` object.
-        
+
         Arguments:
-        
+
         - *minconf* -- Minimum number of confirmations before payments are included.
         - *count* -- Number of transactions to return.
         - *address* -- Receive address to consider
 
         """
         try:
-            return [TransactionInfo(**x) for x in 
+            return [TransactionInfo(**x) for x in
                  self.proxy.listtransactions(account, count)
                  if address is None or x["address"] == address]
         except JSONRPCException,e:
@@ -356,7 +384,7 @@ class BitcoinConnection(object):
     def backupwallet(self, destination):
         """
         Safely copies ``wallet.dat`` to *destination*, which can be a directory or a path with filename.
-        
+
         Arguments:
         - *destination* -- directory or path with filename to backup wallet to.
 
@@ -371,21 +399,21 @@ class BitcoinConnection(object):
         Validate a bitcoin address and return information for it.
 
         The information is represented by a :class:`~bitcoinrpc.data.AddressValidation` object.
-        
-        Arguments:
-        
-        - *validateaddress* -- Address to validate.
 
+        Arguments: -- Address to validate.
+
+
+        - *validateaddress*
         """
         try:
             return AddressValidation(**self.proxy.validateaddress(validateaddress))
         except JSONRPCException,e:
             raise _wrap_exception(e.error)
-        
+
     def getbalance(self, account=None, minconf=None):
         """
         Get the current balance, either for an account or the total server balance.
-        
+
         Arguments:
         - *account* -- If this parameter is specified, returns the balance in the account.
         - *minconf* -- Minimum number of confirmations required for transferred balance.
@@ -400,19 +428,19 @@ class BitcoinConnection(object):
             return self.proxy.getbalance(*args)
         except JSONRPCException,e:
             raise _wrap_exception(e.error)
-        
+
     def move(self, fromaccount, toaccount, amount, minconf=1, comment=None):
         """
         Move from one account in your wallet to another.
-        
+
         Arguments:
-        
+
         - *fromaccount* -- Source account name.
         - *toaccount* -- Destination account name.
         - *amount* -- Amount to transfer.
         - *minconf* -- Minimum number of confirmations required for transferred balance.
         - *comment* -- Comment to add to transaction log.
-        
+
         """
         try:
             if comment is None:
@@ -424,13 +452,13 @@ class BitcoinConnection(object):
 
     def sendfrom(self, fromaccount, tobitcoinaddress, amount, minconf=1, comment=None, comment_to=None):
         """
-        Sends amount from account's balance to bitcoinaddress. This method will fail 
-        if there is less than amount bitcoins with minconf confirmations in the account's 
-        balance (unless account is the empty-string-named default account; it 
+        Sends amount from account's balance to bitcoinaddress. This method will fail
+        if there is less than amount bitcoins with minconf confirmations in the account's
+        balance (unless account is the empty-string-named default account; it
         behaves like the sendtoaddress method). Returns transaction ID on success.
-        
+
         Arguments:
-        
+
         - *fromaccount* -- Account to send from.
         - *tobitcoinaddress* -- Bitcoin address to send to.
         - *amount* -- Amount to send (float, rounded to the nearest 0.01).
@@ -451,14 +479,14 @@ class BitcoinConnection(object):
 
     def getwork(self, data=None):
         """
-        Get work for remote mining, or submit result. 
-        If data is specified, the server tries to solve the block 
+        Get work for remote mining, or submit result.
+        If data is specified, the server tries to solve the block
         using the provided data and returns :const:`True` if it was successful.
-        If not, the function returns formatted hash data (:class:`~bitcoinrpc.data.WorkItem`) 
+        If not, the function returns formatted hash data (:class:`~bitcoinrpc.data.WorkItem`)
         to work on.
-        
+
         Arguments:
-        
+
         - *data* -- Result from remote mining.
 
         """
@@ -470,4 +498,5 @@ class BitcoinConnection(object):
                 return self.proxy.getwork(data)
         except JSONRPCException,e:
             raise _wrap_exception(e.error)
+
 
