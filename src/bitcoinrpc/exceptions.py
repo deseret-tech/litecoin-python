65b0558aa5bdecee00f43d883e96b81547b0d850
Wladimir J. van der Laan <laanwj@gmail.com>
2012-10-05 18:32:23 +0200

    Use RPC error codes from bitcoin core source
    Old exception names are kept for backwards compatibility

--- src/bitcoinrpc/exceptions.py
+++ src/bitcoinrpc/exceptions.py
@@ -26,52 +26,59 @@ class BitcoinException(Exception):
     
     - *code* -- Error code from ``bitcoind``.
     """
-    # Bitcoin specific error codes
-    GENERIC = -1
-    SAFEMODE = -2
-    INVALID_AMOUNT = -3
-    SEND_ERROR = -4
-    INVALID_TRANSACTION_ID = -5
-    INSUFFICIENT_FUNDS = -6
-    OUT_OF_MEMORY = -7
-    INVALID_PARAMETER = -8
-    NOT_CONNECTED = -9
-    DOWNLOADING_BLOCKS = -10
-    # General JSON error codes
-    PARSE_ERROR = -32700
-    MISSING_METHOD = -32600
-    METHOD_MUST_BE_STRING = -32600
-    PARAMS_MUST_BE_ARRAY = -32600
-    METHOD_NOT_FOUND = -32601
-    
+    # Standard JSON-RPC 2.0 errors
+    INVALID_REQUEST  = -32600,
+    METHOD_NOT_FOUND = -32601,
+    INVALID_PARAMS   = -32602,
+    INTERNAL_ERROR   = -32603,
+    PARSE_ERROR      = -32700,
+
+    # General application defined errors
+    MISC_ERROR                  = -1  # std::exception thrown in command handling
+    FORBIDDEN_BY_SAFE_MODE      = -2  # Server is in safe mode, and command is not allowed in safe mode
+    TYPE_ERROR                  = -3  # Unexpected type was passed as parameter
+    INVALID_ADDRESS_OR_KEY      = -5  # Invalid address or key
+    OUT_OF_MEMORY               = -7  # Ran out of memory during operation
+    INVALID_PARAMETER           = -8  # Invalid, missing or duplicate parameter
+    DATABASE_ERROR              = -20 # Database error
+    DESERIALIZATION_ERROR       = -22 # Error parsing or validating structure in raw format
+
+    # P2P client errors
+    CLIENT_NOT_CONNECTED        = -9  # Bitcoin is not connected
+    CLIENT_IN_INITIAL_DOWNLOAD  = -10 # Still downloading initial blocks
+
+    # Wallet errors
+    WALLET_ERROR                = -4  # Unspecified problem with wallet (key not found etc.)
+    WALLET_INSUFFICIENT_FUNDS   = -6  # Not enough funds in wallet or account
+    WALLET_INVALID_ACCOUNT_NAME = -11 # Invalid account name
+    WALLET_KEYPOOL_RAN_OUT      = -12 # Keypool ran out, call keypoolrefill first
+    WALLET_UNLOCK_NEEDED        = -13 # Enter the wallet passphrase with walletpassphrase first
+    WALLET_PASSPHRASE_INCORRECT = -14 # The wallet passphrase entered was incorrect
+    WALLET_WRONG_ENC_STATE      = -15 # Command given in wrong wallet encryption state (encrypting an encrypted wallet etc.)
+    WALLET_ENCRYPTION_FAILED    = -16 # Failed to encrypt the wallet
+    WALLET_ALREADY_UNLOCKED     = -17 # Wallet is already unlocked
+ 
     def __init__(self, error):
         Exception.__init__(self, error['message'])
         self.code = error['code']
 
+##### General application defined errors 
 class SafeMode(BitcoinException):
     """
     Operation denied in safe mode (run ``bitcoind`` with ``-disablesafemode``).
     """
 
-class InvalidAmount(BitcoinException):
+class JSONTypeError(BitcoinException):
     """
-    Invalid amount.
+    Unexpected type was passed as parameter
     """
-        
-class SendError(BitcoinException):
-    """
-    Error while sending coins.
-    """
+InvalidAmount = JSONTypeError # Backwards compatibility
 
-class InvalidTransactionID(BitcoinException):
+class InvalidAddressOrKey(BitcoinException):
     """
-    Invalid transaction ID.
+    Invalid address or key.
     """
-
-class InsufficientFunds(BitcoinException):
-    """
-    Insufficient funds to complete transaction.
-    """
+InvalidTransactionID = InvalidAddressOrKey # Backwards compatibility
 
 class OutOfMemory(BitcoinException):
     """
@@ -80,31 +87,51 @@ class OutOfMemory(BitcoinException):
 
 class InvalidParameter(BitcoinException):
     """
-    Invalid parameter provided to function.
+    Invalid parameter provided to RPC call.
     """
-    
-class NotConnected(BitcoinException):
+
+##### Client errors
+class ClientException(BitcoinException):
+    """
+    P2P network error.
+    This exception is never raised but functions as a superclass
+    for other P2P client exceptions.
+    """
+
+class NotConnected(ClientException):
     """
     Not connected to any peers.
     """
 
-class DownloadingBlocks(BitcoinException):
-     """
+class DownloadingBlocks(ClientException):
+    """
     Client is still downloading blocks.
+    """
+
+##### Wallet errors
+class WalletError(BitcoinException):
+    """
+    Unspecified problem with wallet (key not found etc.)
+    """
+SendError = WalletError # Backwards compatibility
+
+class InsufficientFunds(WalletError):
+    """
+    Insufficient funds to complete transaction.
     """
 
 # For convenience, we define more specific exception classes
 # for the more common errors.
 _exception_map = {
-    BitcoinException.SAFEMODE: SafeMode,
-    BitcoinException.INVALID_AMOUNT: InvalidAmount,
-    BitcoinException.SEND_ERROR: SendError,
-    BitcoinException.INVALID_TRANSACTION_ID: InvalidTransactionID,
-    BitcoinException.INSUFFICIENT_FUNDS: InsufficientFunds,
+    BitcoinException.FORBIDDEN_BY_SAFE_MODE: SafeMode,
+    BitcoinException.TYPE_ERROR: JSONTypeError,
+    BitcoinException.WALLET_ERROR: WalletError,
+    BitcoinException.INVALID_ADDRESS_OR_KEY: InvalidAddressOrKey,
+    BitcoinException.WALLET_INSUFFICIENT_FUNDS: InsufficientFunds,
     BitcoinException.OUT_OF_MEMORY: OutOfMemory,
     BitcoinException.INVALID_PARAMETER: InvalidParameter,
-    BitcoinException.NOT_CONNECTED: NotConnected,
-    BitcoinException.DOWNLOADING_BLOCKS: DownloadingBlocks
+    BitcoinException.CLIENT_NOT_CONNECTED: NotConnected,
+    BitcoinException.CLIENT_IN_INITIAL_DOWNLOAD: DownloadingBlocks
 }
 
 def _wrap_exception(error):
