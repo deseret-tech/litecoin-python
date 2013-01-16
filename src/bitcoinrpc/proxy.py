deseret-tech/litecoin-python

commit a8b9b97357fd678cebd68050a2bae48f90ab6679

There isn't always an error key in resp dict
There isn't always an error key in resp dict particulary if there is no error

src/bitcoinrpc/proxy.py
@@ -1,4 +1,3 @@
-
 """
   Copyright 2011 Jeff Garzik
 
@@ -102,8 +101,9 @@ class AuthServiceProxy(object):
          resp = httpresp.read()
          resp = resp.decode('utf8')
          resp = json.loads(resp, parse_float=decimal.Decimal)
-         if resp['error'] != None:
-             raise JSONRPCException(resp['error'])
+         if 'error' in resp:
+             if resp['error'] != None:
+                 raise JSONRPCException(resp['error'])
          elif 'result' not in resp:
              raise JSONRPCException({
                      'code' : -343, 'message' : 'missing JSON-RPC result'})

commit 0c899d5887d93775fe310d0436a83a7d14eb304d

fix bug when rpc reply error is null in json
fix bug when rpc reply error is null in json, result would not be
returned, even though no error happened. Bug originally introduced in
commit a8b9b97357fd678cebd68050a2bae48f90ab6679

src/bitcoinrpc/proxy.py
@@ -101,9 +101,8 @@ class AuthServiceProxy(object):
          resp = httpresp.read()
          resp = resp.decode('utf8')
          resp = json.loads(resp, parse_float=decimal.Decimal)
-         if 'error' in resp:
-             if resp['error'] != None:
-                 raise JSONRPCException(resp['error'])
+         if 'error' in resp and resp['error'] != None:
+             raise JSONRPCException(resp['error'])
          elif 'result' not in resp:
              raise JSONRPCException({
                      'code' : -343, 'message' : 'missing JSON-RPC result'})